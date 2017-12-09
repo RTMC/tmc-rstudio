59fced0d465eaf9b0f1d1c82728a9167692f9945
eerojala <eero.ojala@helsinki.fi>
2017-12-09 22:13:59 +0200

    Initial roxygen documentation of functions

--- tmcrstudioaddin/R/Exercises.R
+++ tmcrstudioaddin/R/Exercises.R
@@ -1,3 +1,10 @@
+#' @title Get the currently open exercise
+#' @description Get the currently open exercise (the exercise which is located at the current working directory).
+#' @usage exerciseFromWd()
+#' @details Checks if the current working directory is a subfolder in the TMC projects -folder.
+#' @return Name of the exercise directory. If the current working directory isn't an exercise folder, returns an
+#' empty string.
+
 # Returns the exercise that is selected as wd.
 # If wd doenst contain an exercise returns "".
 exerciseFromWd <- function() {
@@ -12,15 +19,29 @@ exerciseFromWd <- function() {
   }
 }
 
+#' @title Get a list of downloaded exercises
+#' @description Get a list of the names of downloaded exercises.
+#' @usage downloadedExercises()
+#' @return List containing the names of the exercises.
 #Returns a list of downloaded exercises
 downloadedExercises <- function() {
   return(c("", list.dirs(path = get_projects_folder(), full.names = FALSE, recursive = FALSE)))
 }
 
+#' @title Get a file path to the specified exercise
+#' @description Get a file path to the specified exercise.
+#' @usage getExercisePath(exercise)
+#' @param exercise Name of the exercise
+#' @return File path to the exercise which name was given as a parameter
+#' @examples getExercisePath("0_0_helloworld")
 getExercisePath <- function(exercise) {
   return(paste0(get_projects_folder(), "/", exercise))
 }
 
+#' @title Source exercise .R files
+#' @description Source the .R files contained in the exercise directory's \code{R} folder.
+#' @usage sourceExercise(exercise)
+#' @param exercise Name of the exercise which .R files are sourced
 sourceExercise <- function(exercise) {
   env <- new.env()
   for (file in list.files(pattern = "[.]R$", path = paste0(getExercisePath(exercise), "/R"),
